2d2928d31c9fc7741f0cc5c4672877fd973f2307
JeongYunSung <123dbstjd@naver.com>
2022-07-12 21:30:21 +0900

    add: banking system

--- src/github.com/jys/learngo/main.go
+++ src/github.com/jys/learngo/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/jys/learngo/banking"
+	"github.com/jys/learngo/dict"
 )
 
 func main() {
-	account := banking.NewAccount("John", 1000)
-	account1 := banking.NewAccount("John", 1000)
-	fmt.Println(*account, *account1)
+	dictionary := dict.Dictionary{"hello": "안녕하세요", "bye": "들어가세요"}
+	dictionary["bye"] = "안녕히 가세요"
+	dictionary.Add("hi", "안녕")
+	fmt.Println(dictionary)
+	fmt.Println(dictionary.Search("hi"))
+	dictionary1 := dict.Dictionary{"hello": "안녕하세요", "bye": "들어가세요"}
+	fmt.Println(dictionary1)
 }
