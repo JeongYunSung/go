1d684baf4c1e0294071959029a31eb28ae1fe79a
JeongYunSung <123dbstjd@naver.com>
2022-07-12 21:30:21 +0900

    add: banking system

--- src/github.com/jys/learngo/dict/dict.go
+++ src/github.com/jys/learngo/dict/dict.go
@@ -1,1 +1,11 @@
 package dict
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
