862cb7ed32c120126e0ecd5245318f85d515df1a
JeongYunSung <123dbstjd@naver.com>
2022-11-07 16:39:48 +0900

    refactor: 최대 페이지 개수 최적화

--- src/github.com/jys/company/main/main.go
+++ src/github.com/jys/company/main/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"runtime/trace"
@@ -37,80 +36,125 @@ type linkData struct {
 	TransComment string `json:"trans_comment"`
 }
 
+type value struct {
+	index int
+	date  string
+	size  int
+}
+
+var (
+	pageSize = 1000
+	dateList = map[string]int{"202207": 0, "202208": 0, "202209": 0, "202210": 0, "202211": 0}
+)
+
 func main() {
 	f, _ := os.Create("trace.out")
 	trace.Start(f)
 
-	defer f.Close()
-	defer trace.Stop()
-
-	ch := make(chan int)
-	fin := make(chan int)
-
 	now := time.Now()
 
-	for i := 1; i <= 4; i++ {
-		go request(ch, fin)
-		ch <- i
-	}
-
-	for i := 0; i < 4; i++ {
-		<-fin
-	}
+	sizeRequest()
+	mainRequest()
 
 	fmt.Println("time : ", time.Since(now))
+
+	defer f.Close()
+	defer trace.Stop()
 }
 
-func request(ch <-chan int, fin chan<- int) {
-	tmp, _ := template.New("url").Parse("http://api.linkprice.com/affiliate/translist.php?a_id=&auth_key=&yyyymmdd={{.Date}}&page={{.Page}}&per_page=1000")
+func sizeRequest() {
+	ch := make(chan *value)
+	fin := make(chan *value)
 
-	b := bytes.Buffer{}
+	for k := range dateList {
+		go (func(c <-chan *value, f chan<- *value) {
+			v := <-c
+			data := getData(v)
 
-	index := <-ch
+			defer (func() {
+				fin <- &value{0, v.date, (data.ListCount / pageSize) + 1}
+			})()
+		})(ch, fin)
 
-	defer (func() {
-		fin <- index
-	})()
-
-	err3 := tmp.Execute(&b, map[string]string{"Page": strconv.Itoa(index), "Date": "202209"})
-	if err3 != nil {
-		log.Fatal(err3)
+		ch <- &value{1, k, 1}
 	}
 
-	resp, err := http.Get(b.String())
+	for i := 0; i < len(dateList); i++ {
+		value := <-fin
+		dateList[value.date] = value.size
+	}
+}
 
-	if err != nil {
-		log.Fatal(err)
+func mainRequest() {
+	ch := make(chan *value)
+	fin := make(chan int)
+
+	for k := range dateList {
+		for index := 1; index <= getSumPage(k); index++ {
+			go request(ch, fin)
+			ch <- &value{index, k, pageSize}
+		}
 	}
 
-	r, _ := io.ReadAll(resp.Body)
-
-	str := string(r)
-	res := response{}
-
-	err2 := json.Unmarshal([]byte(str), &res)
-
-	if err2 != nil {
-		log.Fatal(err2)
+	for k := range dateList {
+		for index := 1; index <= getSumPage(k); index++ {
+			<-fin
+		}
 	}
+}
 
-	data := make(map[string][]linkData)
+func getSumPage(key string) int {
+	sumPage := 0
+	for k, v := range dateList {
+		if k == key {
+			sumPage += v
+		}
+	}
+	return sumPage
+}
+
+func request(ch <-chan *value, fin chan<- int) {
+	data := <-ch
+
+	res := getData(data)
+
+	orders := make(map[string][]linkData)
 
 	for _, order := range res.OrderList {
-		data[order.UserId] = append(data[order.UserId], order)
+		orders[order.OCd] = append(orders[order.OCd], order)
 	}
 
-	fmt.Printf("%d 페이지의 %d 건의 데이터 조회 완료 \n", index, len(res.OrderList))
-
-	for k, v := range data {
-		if len(v) > 1 {
-			fmt.Println("==========================================")
-			for _, order := range v {
+	for k, v := range orders {
+		for _, order := range v {
+			if order.OCd == "2305559427" {
 				fmt.Printf("user_id : %s, m_id : %s, trlog_id : %s, order_code : %s, p_code : %s, p_name : %s\ncount : %s, sales : %d, commission : %d, status : %s, date: %s comment : %s \n",
 					k, order.MId, order.TrlogId, order.OCd, order.PCd, order.PNm, order.ItCnt, order.Sales, order.Commission, order.Status, order.Yyyymmdd+order.Hhmiss, order.TransComment)
 			}
 		}
 	}
 
-	fmt.Println("[JYS]중복 체크 로직")
+	defer (func() {
+		fin <- 1
+	})()
+}
+
+func getData(data *value) response {
+	resp, _ := http.Get(getURL(data))
+	body, _ := io.ReadAll(resp.Body)
+
+	str := string(body)
+	res := response{}
+
+	json.Unmarshal([]byte(str), &res)
+
+	return res
+}
+
+func getURL(data *value) string {
+	tmp, _ := template.New("url").Parse("http://api.linkprice.com/affiliate/translist.php?a_id=&auth_key=&yyyymmdd={{.Date}}&page={{.Page}}&per_page={{.PerPage}}")
+
+	b := bytes.Buffer{}
+	tmp.Execute(&b, map[string]string{"Page": strconv.Itoa(data.index), "Date": data.date, "PerPage": strconv.Itoa(data.size)})
+
+	return b.String()
 }
