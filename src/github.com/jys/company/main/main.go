3bca3d514e16a4578315b853bc9df4cacfa33cb6
JeongYunSung <123dbstjd@naver.com>
2022-11-07 14:15:40 +0900

    reafactor: 공유자원 동기모델 -> CSP 모델로 로직 변경

--- src/github.com/jys/company/main/main.go
+++ src/github.com/jys/company/main/main.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"runtime/trace"
 	"strconv"
-	"sync"
+	"text/template"
+	"time"
 )
 
 type response struct {
@@ -36,74 +38,79 @@ type linkData struct {
 }
 
 func main() {
-	group := sync.WaitGroup{}
+	f, _ := os.Create("trace.out")
+	trace.Start(f)
+
+	defer f.Close()
+	defer trace.Stop()
+
+	ch := make(chan int)
+	fin := make(chan int)
+
+	now := time.Now()
 
 	for i := 1; i <= 4; i++ {
-		group.Add(1)
-		go (func(index int) {
-			tmp, _ := template.New("url").Parse("http://api.linkprice.com/affiliate/translist.php?a_id=&auth_key=&yyyymmdd={{.Date}}&page={{.Page}}&per_page=1000")
-
-			b := bytes.Buffer{}
-
-			err3 := tmp.Execute(&b, map[string]string{"Page": strconv.Itoa(index), "Date": "202209"})
-			if err3 != nil {
-				log.Fatal(err3)
-			}
-
-			resp, err := http.Get(b.String())
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			r, _ := io.ReadAll(resp.Body)
-
-			str := string(r)
-			res := response{}
-
-			err2 := json.Unmarshal([]byte(str), &res)
-
-			if err2 != nil {
-				log.Fatal(err2)
-			}
-
-			data := make(map[string][]linkData)
-			data3 := map[string]int{}
-
-			for _, order := range res.OrderList {
-				data[order.UserId] = append(data[order.UserId], order)
-				if data3[order.OCd] == 0 {
-					data3[order.OCd] = 1
-				} else {
-					data3[order.OCd] = data3[order.OCd] + 1
-				}
-			}
-
-			fmt.Printf("%d 페이지의 %d 건의 데이터 조회 완료 \n", index, len(res.OrderList))
-
-			for k, v := range data {
-				if len(v) > 1 {
-					fmt.Println("==========================================")
-					for _, order := range v {
-						fmt.Printf("user_id : %s, m_id : %s, trlog_id : %s, order_code : %s, p_code : %s, p_name : %s\ncount : %s, sales : %d, commission : %d, status : %s, date: %s comment : %s \n",
-							k, order.MId, order.TrlogId, order.OCd, order.PCd, order.PNm, order.ItCnt, order.Sales, order.Commission, order.Status, order.Yyyymmdd+order.Hhmiss, order.TransComment)
-					}
-				}
-			}
-
-			fmt.Println("[JYS]중복 체크 로직")
-
-			for k, v := range data3 {
-				if v > 1 {
-					fmt.Println("==========================================")
-					fmt.Println("중복된 order_code : ", k, " count : ", v)
-				}
-			}
-
-			defer group.Done()
-			defer resp.Body.Close()
-		})(i)
+		go request(ch, fin)
+		ch <- i
 	}
 
-	group.Wait()
+	for i := 0; i < 4; i++ {
+		<-fin
+	}
+
+	fmt.Println("time : ", time.Since(now))
+}
+
+func request(ch <-chan int, fin chan<- int) {
+	tmp, _ := template.New("url").Parse("http://api.linkprice.com/affiliate/translist.php?a_id=&auth_key=&yyyymmdd={{.Date}}&page={{.Page}}&per_page=1000")
+
+	b := bytes.Buffer{}
+
+	index := <-ch
+
+	defer (func() {
+		fin <- index
+	})()
+
+	err3 := tmp.Execute(&b, map[string]string{"Page": strconv.Itoa(index), "Date": "202209"})
+	if err3 != nil {
+		log.Fatal(err3)
+	}
+
+	resp, err := http.Get(b.String())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, _ := io.ReadAll(resp.Body)
+
+	str := string(r)
+	res := response{}
+
+	err2 := json.Unmarshal([]byte(str), &res)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	data := make(map[string][]linkData)
+
+	for _, order := range res.OrderList {
+		data[order.UserId] = append(data[order.UserId], order)
+	}
+
+	fmt.Printf("%d 페이지의 %d 건의 데이터 조회 완료 \n", index, len(res.OrderList))
+
+	for k, v := range data {
+		if len(v) > 1 {
+			fmt.Println("==========================================")
+			for _, order := range v {
+				fmt.Printf("user_id : %s, m_id : %s, trlog_id : %s, order_code : %s, p_code : %s, p_name : %s\ncount : %s, sales : %d, commission : %d, status : %s, date: %s comment : %s \n",
+					k, order.MId, order.TrlogId, order.OCd, order.PCd, order.PNm, order.ItCnt, order.Sales, order.Commission, order.Status, order.Yyyymmdd+order.Hhmiss, order.TransComment)
+			}
+		}
+	}
+
+	fmt.Println("[JYS]중복 체크 로직")
 }
